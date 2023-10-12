c1242adb8341580795428a7a2350adb9658addf4
aron_lds <sud11real@gmail.com>
2023-10-12 09:18:17 -0500

    Nuevas dependencias

--- src/main/java/com/project/micro/creditpayment/CreditpaymentApplication.java
+++ src/main/java/com/project/micro/creditpayment/CreditpaymentApplication.java
@@ -2,7 +2,9 @@ package com.project.micro.creditpayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CreditpaymentApplication {
 
